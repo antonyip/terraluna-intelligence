9c6f043729789fd7e001fd6f885afe83d6946e6a
Anton Yip <anton_yip@hotmail.com>
2022-03-23 19:39:44 +0800

    upload todo

--- pages/index.js
+++ pages/index.js
@@ -1654,16 +1654,6 @@ function PermanentDrawerLeft() {
                 ,['Mars', '/mars.jpeg']
                 ,['RiskHarbor', '/RH.jpg']
                 ,['LFG', '/LFG.jpg']
-                
-                // governance https://app.flipsidecrypto.com/dashboard/terra-146-whale-voters-k56HKq
-                // loop https://app.flipsidecrypto.com/dashboard/whale-dependency-index-IqUTca
-                // astroport price https://app.flipsidecrypto.com/dashboard/lockdrops-keep-fallin-on-my-head-txvCxH
-                // bridge in https://app.flipsidecrypto.com/dashboard/bridge-then-anchor-WZMBAJ
-                // stader https://app.flipsidecrypto.com/velocity/collections/d207f92a-0455-454e-a6b0-8444ba179246
-                // tfloki https://app.flipsidecrypto.com/velocity/queries/5967c8f2-eb21-4de2-b760-aaec5e5992d8
-                // pylon https://app.flipsidecrypto.com/velocity/collections/4aaac826-5678-4338-b19a-98c967118e70
-                // ldo https://app.flipsidecrypto.com/velocity/collections/b62badb5-1698-4205-a613-6a32b5c92b58
-                // galatic grds https://app.flipsidecrypto.com/velocity/queries/f3aa6a75-6ce5-4b8b-ac01-ec8272bd7164
               ].sort().map( n => {
                 return (
                   <ListItem button key={n[0]} onClick={() => setPage(n[0])}>
@@ -1688,7 +1678,6 @@ function PermanentDrawerLeft() {
               ,'Pylon'
               ,'Prism'
               ,'StarTerra'
-              ,'Kuji'
               ,'VKR'
               ,'Apollo'
               ,'Orion'
